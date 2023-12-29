e404b59713f478e109dc42b92b891bf08b6e7c92
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-29 18:47:09 +0100

    components link

--- app/page.js
+++ app/page.js
@@ -1,14 +1,54 @@
 import Link from 'next/link';
 
+import ImageSlideshow from '../components/images/image-slideshow';
+import classes from './page.module.css'
+
 export default function Home() {
   return (
+   <>
+    <header className={classes.show}>
+      <div className={classes.slideshow}>
+       <ImageSlideshow />
+
+      </div>
+      <div className={classes.hero}>
+        <h1>NEXTLEVEL Food For NEXTLEVEL Foodies</h1>
+        <p>Taste & share meals aruond the globe</p>
+      </div>
+      <div className={classes.cta}>
+        <Link href='/community'>Link with the Community</Link>
+        <Link href='/meals'>Scrutinize meals</Link>
+      </div>
+    </header>
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meals</Link></p>
-      <p><Link href="/community">Community</Link></p>
+    <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </main>
+   </>
   );
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,27 +1,37 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+
+import MainHeaderBackground from './main-header-background';
 import logoImg from '../assets/logo.png';
 import classes from './main-header.module.css';
+import NavLink from './nav-link';
+
 
 
 export default function MainHeader(){
+   
     return(
+        <>
+        <MainHeaderBackground />
         <header className={classes.header}>
         <Link className={classes.logo} href="/">
-            <Image src={logoImg.src}  width={500}
-        height={500} alt='A plate with food on it' />
+            <Image src={logoImg} 
+         alt='A plate with food on it' priority />
             NEXT LEVEL FOOD
         </Link>
 
         <nav className={classes.nav}>
             <ul>
                 <li>
-                <Link href="/meals">Browse meals</Link>
-                    <Link href="/community">Foodies Commmunity</Link>
+                    <NavLink href="/meals">Browse Meals</NavLink>
+                </li>
+                <li>
+                <NavLink href="/community">Foodies Community</NavLink>
                 </li>
             </ul>
         </nav>
         </header>
-    )
+        </>
+    );
 }
